b5f95a4d6c3564262b1867f72ef4c3c802438692
Zongru Li <zongruli@outlook.jp>
2024-10-09 10:23:53 +0300

    Add files via upload

--- flight_game_backup.sql
+++ flight_game_backup.sql
@@ -19,7 +19,7 @@
 --
 -- Table structure for table `airport`
 --
-create database flight_game if not EXISTS
+create database flight_game if not EXISTS;
 use databse flight_game;
 DROP TABLE IF EXISTS `airport`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
